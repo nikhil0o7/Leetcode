d7b14ea06ef24621283ad06bb4679a23138d17f5
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2023-08-06 18:49:58 -0400

    Time: 100 ms (94.14%), Space: 16.3 MB (81.88%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,9 +1,33 @@
-class Solution(object):
-    def isValidSudoku(self, board):
-        res = []
-        for i in range(9):
-            for j in range(9):
-                element = board[i][j]
-                if element != '.':
-                    res += [(i, element), (element, j), (i // 3, j // 3, element)]
-        return len(res) == len(set(res))
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        N = 9
+
+        # Use hash set to record the status
+        rows = [set() for _ in range(N)]
+        cols = [set() for _ in range(N)]
+        boxes = [set() for _ in range(N)]
+
+        for r in range(N):
+            for c in range(N):
+                val = board[r][c]
+                # Check if the position is filled with number
+                if val == ".":
+                    continue
+
+                # Check the row
+                if val in rows[r]:
+                    return False
+                rows[r].add(val)
+
+                # Check the column
+                if val in cols[c]:
+                    return False
+                cols[c].add(val)
+
+                # Check the box
+                idx = (r // 3) * 3 + c // 3
+                if val in boxes[idx]:
+                    return False
+                boxes[idx].add(val)
+
+        return True
