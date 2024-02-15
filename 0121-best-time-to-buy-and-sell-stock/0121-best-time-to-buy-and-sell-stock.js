41d95b114887230e8c1b5251914f73186f35e4ad
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2024-02-15 10:35:38 -0500

    Time: 126 ms (8.36%), Space: 78.8 MB (5.23%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,17 +3,33 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let min_price = prices[0]
-    let max_profit = 0
-    for(let i=0; i<prices.length;i++){
-        let temp_price = prices[i];
-        if (max_profit < temp_price- min_price){
-            max_profit = temp_price - min_price;
-        } else if (temp_price < min_price){
-            min_price = temp_price
+    let min_prices = prices[0];
+    let max_profit = 0;
+    for(let i in prices){
+        let temp = prices[i];
+        if(max_profit < temp - min_prices){
+            max_profit = temp - min_prices;
+        }else if(temp < min_prices){
+            min_prices = temp;
         }
     }
     
     return max_profit;
     
 };
+
+// var maxProfit = function(prices) {
+//     let min_price = prices[0]
+//     let max_profit = 0
+//     for(let i=0; i<prices.length;i++){
+//         let temp_price = prices[i];
+//         if (max_profit < temp_price- min_price){
+//             max_profit = temp_price - min_price;
+//         } else if (temp_price < min_price){
+//             min_price = temp_price
+//         }
+//     }
+    
+//     return max_profit;
+    
+// };
